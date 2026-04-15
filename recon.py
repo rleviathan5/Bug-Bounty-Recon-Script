29bda1242fe7227afe7dc2c0e30364d26623b369
rleviathan5 <corkyn2005@gmail.com>
2026-04-15 15:08:14 +0100

    added os module for checking if namp is installed on the system

--- recon.py
+++ recon.py
@@ -1,7 +1,11 @@
 import subprocess
+import os
+
+if os.path.exists('nmap'):
+    print('exists')
 
 nmap_command = subprocess.run(['nmap', '-h'], capture_output=True, text=True)
 gospider_command = subprocess.run(['gospider', '-h'], capture_output=True, text=True)
 gobuster_command = subprocess.run(['gobuster', '-h'], capture_output=True, text=True)
 
-print(nmap_command.stdout)
+#print(nmap_command.stdout)
