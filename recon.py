a205748179731dc2b909d626252c2ff4f5224487
rleviathan5 <corkyn2005@gmail.com>
2026-04-15 15:19:56 +0100

    added checks for gobuster and gospider

--- recon.py
+++ recon.py
@@ -1,11 +1,22 @@
 import subprocess
 import os
 
-path = '/usr/bin/nmap'
-if os.path.exists(path):
-    print('Nmap exists')
-else:
-    print('nope')
+def check_for_tools_linux():
+    path = '/usr/bin/'
+    if os.path.exists(path + 'nmap'):
+        print('Nmap exists')
+    else:
+        print('nope')
+
+    if os.path.exists(path + 'gospider'):
+        print('Gospider exists')
+    else:
+        print('nope')
+
+    if os.path.exists(path + 'gobuster'):
+        print('Gobuster exists')
+    else:
+        print('nope')
 
 nmap_command = subprocess.run(['nmap', '-h'], capture_output=True, text=True)
 gospider_command = subprocess.run(['gospider', '-h'], capture_output=True, text=True)
