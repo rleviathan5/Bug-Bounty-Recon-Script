0085dae10d89c67f0afc8def70ffeb89b3879f37
rleviathan5 <corkyn2005@gmail.com>
2026-04-15 14:58:24 +0100

    added subprocesses for all 3 tools

--- Recon Script.py
+++ Recon Script.py
@@ -1,5 +1,7 @@
 import subprocess
 
 nmap_command = subprocess.run(['nmap', '-h'], capture_output=True, text=True)
+gospider_command = subprocess.run(['gospider', '-h'], capture_output=True, text=True)
+gobuster_command = subprocess.run(['gobuster', '-h'], capture_output=True, text=True)
 
 print(nmap_command.stdout)
